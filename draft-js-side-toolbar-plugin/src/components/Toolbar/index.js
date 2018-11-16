deb2898c4ffbd3c1f85b9b834fcf779ecfc35f15
Henry L <meng.foo.bar@gmail.com>
2018-11-16 15:12:28 -0500

    modified sidetoolbar to work with autosuggest

--- draft-js-side-toolbar-plugin/src/components/Toolbar/index.js
+++ draft-js-side-toolbar-plugin/src/components/Toolbar/index.js
@@ -3,31 +3,19 @@ import React from 'react';
 import DraftOffsetKey from 'draft-js/lib/DraftOffsetKey';
 
 export default class Toolbar extends React.Component {
-
+  //TODO: add prop for x, y offset to support other sidebar types as well
   state = {
     position: {
-      transform: 'scale(0)',
     }
   }
 
-  componentDidMount() {
-    this.props.store.subscribeToItem('editorState', this.onEditorStateChange);
+  componentWillMount() {
+    this.computePosition()
   }
 
-  componentWillUnmount() {
-    this.props.store.unsubscribeFromItem('editorState', this.onEditorStateChange);
-  }
-
-  onEditorStateChange = (editorState) => {
+  computePosition = () => {
+    const {editorState} = this.props;
     const selection = editorState.getSelection();
-    if (!selection.getHasFocus()) {
-      this.setState({
-        position: {
-          transform: 'scale(0)',
-        },
-      });
-      return;
-    }
 
     const currentContent = editorState.getCurrentContent();
     const currentBlock = currentContent.getBlockForKey(selection.getStartKey());
@@ -36,15 +24,14 @@ export default class Toolbar extends React.Component {
     // Note: need to wait on tick to make sure the DOM node has been create by Draft.js
     setTimeout(() => {
       const node = document.querySelectorAll(`[data-offset-key="${offsetKey}"]`)[0];
-      const top = node.getBoundingClientRect().top;
-      const editor = this.props.store.getItem('getEditorRef')().refs.editor;
-      const scrollY = window.scrollY == null ? window.pageYOffset : window.scrollY;
+      const {top, bottom} = node.getBoundingClientRect();
+
+      const median = (bottom-top)/2
+      const editor = this.props.getEditorRef().editor.editor;
       this.setState({
         position: {
-          top: (top + scrollY),
-          left: editor.getBoundingClientRect().left - 80,
-          transform: 'scale(1)',
-          transition: 'transform 0.15s cubic-bezier(.3,1.2,.2,1)',
+          top: (node.offsetTop + median ),
+          left: editor.getBoundingClientRect().left - 320,
         },
       });
     }, 0);
@@ -57,14 +44,9 @@ export default class Toolbar extends React.Component {
         className={theme.toolbarStyles.wrapper}
         style={this.state.position}
       >
-        {this.props.structure.map((Component, index) => (
-          <Component
-            key={index}
-            getEditorState={store.getItem('getEditorState')}
-            setEditorState={store.getItem('setEditorState')}
-            theme={theme}
-          />
-        ))}
+        {
+          this.props.children
+        }
       </div>
     );
   }
